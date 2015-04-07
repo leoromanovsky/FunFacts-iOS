0b747ee77aba8fd7e9ceb709548586601288dbb0
Leo Romanovsky <leoromanovsky@gmail.com>
2015-04-06 23:42:33 -0700

    Change the text!

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLab: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,9 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
     @IBAction func showFunFact() {
         println("You pressed the button!")
+        
+        funFactLab.text = "lol"
     }
 }
 
